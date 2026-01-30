4877c253c18ab3f272a61d98b42b8da5b4d0f478
siddharathcodes <siddharathcodes@gmail.com>
2026-01-30 19:29:15 +0545

    now changing mongodb url in env

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function createdatabase(){
-   await mongoose.connect('mongodb+srv://siddharathcodes_db_user:WFXZMDZGxocirPRG@cluster0.cjsxxvf.mongodb.net/?appName=Cluster0');
+   await mongoose.connect(MONGODB_URL);
     console.log('database has been connected');
 }
 
